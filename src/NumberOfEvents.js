9ccd3417624a57c92f30edc2d11abfdab269f75d
a17labs <*a17matte@gmail.com*>
2021-10-13 20:20:56 +0100

    changes

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -6,18 +6,34 @@ class NumberOfEvents extends Component {
     infoText: "",
   };
 
-  render() {
-    const numberOfEvents = this.state.numberOfEvents;
-    return (
-      <div className="numberOfEvents">
-        <input
-          type="text"
-          className="EventsNumber"
-          value={numberOfEvents}
-        />
+  updateEventCount = (eventCount) => {
+    if(eventCount < 1 || eventCount > 32) {
+    return this.setState({
+      numberOfEvents: 0,
+      errorText: 'Please select a number between 1-32'
+    });
+  } else {
+    this.setState({
+      numberOfEvents: eventCount,
+      errorText: ''
+    });
+  }
+    // this.props.updateEventCount(eventCount);
+  };
 
+  render() {
+    return (
+      <div className="number-of-events">
+        <p className="resultNr">Number of results:</p>
+        <input 
+          type="number"
+          className="eventNumber"
+          value={this.props.numberOfEvents}
+          onChange={(e) => this.updateEventCount(e.target.value)}
+        />
       </div>
-    );
+    )
   }
 }
+
 export default NumberOfEvents;
--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -2,15 +2,24 @@ import React from "react";
 import { shallow } from "enzyme";
 import NumberOfEvents from "../NumberOfEvents";
 
-
-describe("<NumberOfEvents /> component", () => {
+describe('<NumberOfEvents /> component', () => {
   let NumberOfEventsWrapper;
   beforeAll(() => {
-    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+    NumberOfEventsWrapper = shallow(<NumberOfEvents updateEventCount={() => {}} />)
   });
 
-  test("textbox is rendered", () => {
-    expect(NumberOfEventsWrapper.find(".EventsNumber")).toHaveLength(1);
+  test('render NumberOfEvents', () => {
+    expect(NumberOfEventsWrapper.find('.number-of-events')).toHaveLength(1);
   });
 
+  test('render 8 events', () => {
+    const numberOfEvents = NumberOfEventsWrapper.prop('numberOfEvents');
+    expect(NumberOfEventsWrapper.find('.eventNumber').prop('value')).toBe(numberOfEvents);
+  });
+
+  test('change number of events with input value', () => {
+    const inputValue = { target: { value: 20}};
+    NumberOfEventsWrapper.find('.eventNumber').simulate('change', inputValue);
+    expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(20);
+  });
 });
