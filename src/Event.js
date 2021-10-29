4baf31e417fa7fd8c8bea081fe8d2416bee99c7f
a17labs <*a17matte@gmail.com*>
2021-10-29 16:36:25 +0100

    changes

--- src/Event.js
+++ src/Event.js
@@ -3,67 +3,43 @@
 import React, { Component } from "react";
 
 class Event extends Component {
-  state = {
-    collapsed: true,
-  };
+state = {
+collapsed: false,
+};
 
-  handleClickOnShowDetails = () => {
-    this.setState({
-      collapsed: false,
-    });
-  };
+handleClickOnShowDetails = () => {
+if (this.state.collapsed === true) {
+this.setState({ collapsed: false });
+} else {
+this.setState({ collapsed: true });
+}
+};
 
-  handleClickOnHideDetails = () => {
-    this.setState({
-      collapsed: true,
-    });
-  };
+render() {
+const { event } = this.props;
+return (
+<div className="event">
+<h2 className="summary">{event.summary}</h2>
+<p className="start-date">
+{event.start.dateTime} ({event.start.timeZone})
+</p>
 
-  render() {
-    const { event } = this.props;
-    return (
-      <div className="event">
-        <h2 className="summary">{event.summary}</h2>
-        <p className="start-date">
-          {event.start.dateTime} ({event.start.timeZone})
-        </p>
-        <p className="location">
-          @{event.summary} | {event.location}
-        </p>
-       
-       
-        <button
-          className="show-details-btn"
-          onClick={this.handleClickOnShowDetails}
-        >
-          Show Details
-        </button>
-        
-        
-        <div
-          className={`extra-details ${this.state.collapsed ? "hide" : "show"}`}
-        >
-          <h3>About the event:</h3>
-          <a href={event.htmlLink} role="noopener" target="_blank">
-            See details on Google Calendar
-          </a>
-          <p className="event-description">{event.description}</p>
-          
-          
-          
-          <button
-            className="hide-details-btn"
-            onClick={this.handleClickOnHideDetails}
-          >
-            Hide Details
-          </button>
+{this.state.collapsed && (
+<div className='eventDetails'>
+<h2>About event:</h2>
+<p>{event.description}</p>
+</div>
+)}
 
-
-
-        </div>
-      </div>
-    );
-  }
+<button
+className="show-details-btn"
+onClick={this.handleClickOnShowDetails}
+>
+{!this.state.collapsed ? 'Show Details' : 'Hide Details'}
+</button>
+</div>
+);
+}
 }
 
 export default Event;
